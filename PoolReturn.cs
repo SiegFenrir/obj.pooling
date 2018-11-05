e5616077e8b792e1b0e3c506135195cc9cd6ce39
SiegFenrir <43831769+SiegFenrir@users.noreply.github.com>
2018-11-05 19:32:16 -0200

    Add files via upload

---
+++ PoolReturn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolReturn : MonoBehaviour
+{
+    public Transform player;
+
+    public float speed;
+
+    Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    //private void Update()
+    //{
+    //    transform.Translate(player.up * speed * Time.deltaTime);
+    //}
+
+    private void FixedUpdate()
+    {
+        //rig.velocity = player.up * speed;
+       // rig.AddForce(player.up * speed*Time.deltaTime,ForceMode.Impulse);
+    }
+
+
+    private void OnEnable()
+    {
+        Invoke("HideMe", 2f);
+    }
+
+    void HideMe()
+    {
+        rig.velocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
---
+++ PoolShooter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolShooter : MonoBehaviour
+{
+    public float fireRate = 1f;
+
+    float tempotiro;
+
+    int estado;
+
+    float force = 200;
+
+    public Transform target,spawn;
+
+    private void Start()
+    {
+        //InvokeRepeating("Fire", fireRate, fireRate);
+    }
+
+  
+
+    private void Update()
+    {
+        
+
+        print(1f / Time.deltaTime);
+        tempotiro += Time.deltaTime;
+
+        if (tempotiro>0.2f)
+        {
+            tempotiro = 0;
+
+            estado = Random.Range(0, 1);
+
+            Fire();
+
+        }
+        spawn.transform.Rotate(0, 0, 8);
+
+        transform.rotation = Quaternion.LookRotation(target.position-transform.position);
+    }
+
+
+    void Fire()
+    {
+        GameObject obj = PoolerScript.current.GetPooledObject();
+
+        if (obj == null) return;
+
+
+        if (estado == 1)
+        {
+            obj.transform.rotation = transform.rotation;
+            force = 200f;
+        }
+        else if (estado == 0)
+        {
+            obj.transform.rotation = transform.rotation;
+            transform.rotation = Quaternion.identity;
+            force = 500f;
+        }
+
+
+        obj.transform.position = transform.position;
+        obj.SetActive(true);
+        if (obj.GetComponent<Rigidbody>().velocity == Vector3.zero)
+            obj.GetComponent<Rigidbody>().AddForce(transform.up * force);
+
+
+
+    }
+}
